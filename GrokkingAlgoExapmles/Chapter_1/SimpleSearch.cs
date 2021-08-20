53755c35638484a9b9217a9d459e948ecca06066
ahmedSamir50 <ahmed.samir.abdelaal@gmail.com>
2021-08-20 16:22:26 +0200

    linear simple search with tests

--- GrokkingAlgoExapmles/Chapter_1/SimpleSearch.cs
+++ GrokkingAlgoExapmles/Chapter_1/SimpleSearch.cs
@@ -9,5 +9,22 @@ namespace GrokkingAlgoExapmles.Chapter_1
     /// </summary>
     public class SimpleSearch
     {
+        /// <summary>
+        /// Linear the search. With O(n)
+        /// no difference if the array is sorted or not 
+        /// </summary>
+        /// <param name="searchIn">The search in. array </param>
+        /// <param name="searchFor">The search for. number/ element</param>
+        /// <param name="trialsCount">The trials count. till finish</param>
+        /// <returns>An int. (-1) if not found or the element index if found </returns>
+        public static int LineSearch (int[] searchIn, int searchFor, out int trialsCount) {
+            trialsCount = 1;
+            for (int i = 0; i < searchIn.Length; i++,trialsCount++)
+            {
+                if (searchIn[i] == searchFor)
+                    return i;
+            }
+            return -1;
+        }
     }
 }
--- GrokkingAlgoTests/ChapterOneTests/SimpleSearchTests.cs
+++ GrokkingAlgoTests/ChapterOneTests/SimpleSearchTests.cs
@@ -7,50 +7,50 @@ using System.Linq;
 
 namespace GrokkingAlgoTests.ChapterOneTests
 {
-    public class BinarySearchTests
+    public class SimpleSearchTests
     {
 
-       
         [Test]
-        public void SortedArrayExistingElementReturnsTheElementIndex () {
-            var result = BinarySearch.BSearch(new int[] {1,2,3,4,5,6,7,8,9,10 } , 10 , out int actualTrials);
+        public void ArrayExistingElementReturnsTheElementIndex () {
+            int[] searchIn = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var result = SimpleSearch.LineSearch(searchIn, 10 , out int actualTrials);
             Console.WriteLine(actualTrials);
-            int _MaxTraialsShouldBe = (int)Math.Log2(10) + 1;
+            int _MaxTraialsShouldBe = searchIn.Length;
             Assert.AreEqual(9,result);
             Assert.IsTrue(_MaxTraialsShouldBe >= actualTrials);
         }
         [Test]
-        public void SortedArrayExistingElementReturnsTheElementIndex2 ()
+        public void ArrayExistingElementReturnsTheElementIndex2 ()
         {
             const int elementsCountInArray = 10000000;
             var arr = Enumerable.Range(10, elementsCountInArray).ToArray(); ;
 
-            var result = BinarySearch.BSearch(arr, 10000009 , out int actualTrials);
-            int _MaxTraialsShouldBe = (int) Math.Log2(elementsCountInArray) + 1;
+            var result = SimpleSearch.LineSearch(arr, 10000009 , out int actualTrials);
+            int _MaxTraialsShouldBe =  arr.Length+1;
            
             Assert.AreEqual(9999999,result);
             Assert.IsTrue(_MaxTraialsShouldBe >= actualTrials);
         }
         [Test]
-        public void TrialsCountInWortstCaseIsLogArrayCountPlusOneIfFoundOrLess () {
+        public void TrialsCountInWortstCaseIsArrayLengthPlusOneIfFoundOrLess () {
             const int elementsCountInArray = 10000000;
             var arr = Enumerable.Range(10, elementsCountInArray).ToArray(); ;
 
-            var result = BinarySearch.BSearch(arr, 10000009, out int actualTrials);
-            int _MaxTraialsShouldBe = (int)Math.Log2(elementsCountInArray) + 1;
+            var result = SimpleSearch.LineSearch(arr, 10000009, out int actualTrials);
+            int _MaxTraialsShouldBe =  arr.Length+1;
 
             Assert.AreEqual(9999999, result);
             Assert.IsTrue(_MaxTraialsShouldBe>= actualTrials);
         }
 
         [Test]
-        public void TrialsCountInWortstCaseIsLogArrayCountPlusOneIfNotFoundOrLessAndReturnsMinusOne ()
+        public void TrialsCountInWortstCaseIsArrayLengthPlusOneIfNotFoundOrLessAndReturnsMinusOne ()
         {
             const int elementsCountInArray = 10000000;
             var arr = Enumerable.Range(10, elementsCountInArray).ToArray(); ;
 
-            var result = BinarySearch.BSearch(arr, 5, out int actualTrials);
-            int _MaxTraialsShouldBe = (int)Math.Log2(elementsCountInArray) + 1;
+            var result = SimpleSearch.LineSearch(arr, 5, out int actualTrials);
+            int _MaxTraialsShouldBe = arr.Length+1;
 
             Assert.AreEqual(-1, result);
             Assert.IsTrue(_MaxTraialsShouldBe >= actualTrials);
