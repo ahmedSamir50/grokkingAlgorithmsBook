5ed4b94e2ccf0aca306c698f80ee1db7c277aa9c
ahmedSamir50 <ahmed.samir.abdelaal@gmail.com>
2021-08-22 17:38:36 +0200

    Selection Sort As Generic

--- GrokkingAlgoExapmles/Chapter_2/SelectionSort.cs
+++ GrokkingAlgoExapmles/Chapter_2/SelectionSort.cs
@@ -7,7 +7,7 @@ namespace GrokkingAlgoExapmles.Chapter_2
     /// <summary>
     /// The selection sort. O(n) of O(n) = O(n^2)
     /// </summary>
-    public class SelectionSort
+    public class SelectionSort<T> : ComparingTBase<T> where T : IComparable<T>
     {
         /// <summary>
         /// Sorts an array Ascending or Descending -*In Place*-
@@ -15,7 +15,7 @@ namespace GrokkingAlgoExapmles.Chapter_2
         /// <param name="toSortArray">The to sort array.</param>
         /// <param name="Asc">If true, Ascending.</param>
         /// <returns> The array Sorted ASC/DESC .</returns>
-        public static int[] SortArray (int[] toSortArray, bool Asc = true)
+        public static T[] SortArray (T[] toSortArray, bool Asc = true)
         {
             for (int i = 0; i < toSortArray.Length; i++)
             {
@@ -25,7 +25,7 @@ namespace GrokkingAlgoExapmles.Chapter_2
                 else
                     cindex = FindMaxIndexFromXToArrayLength(toSortArray, i);
 
-                int temp = toSortArray[cindex];
+                T temp = toSortArray[cindex];
                 toSortArray[cindex] = toSortArray[i];
                 toSortArray[i] = temp;
             }
@@ -37,11 +37,11 @@ namespace GrokkingAlgoExapmles.Chapter_2
         /// </summary>
         /// <param name="toSortArray">The to sort array.</param>
         /// <param name="i">The i.</param>
-        private static int FindSmallestIndexFromXToArrayLength (int[] toSortArray, int i)
+        private static int FindSmallestIndexFromXToArrayLength (T[] toSortArray, int i)
         {
             int minIDX = i;
             for (int j = i + 1; j < toSortArray.Length; j++)
-                if (toSortArray[j] < toSortArray[minIDX])
+                if (IsLessThan( toSortArray[j],  toSortArray[minIDX]))
                     minIDX = j;
             return minIDX;
         }
@@ -51,11 +51,11 @@ namespace GrokkingAlgoExapmles.Chapter_2
         /// </summary>
         /// <param name="toSortArray">The to sort array.</param>
         /// <param name="i">The i.</param>
-        private static int FindMaxIndexFromXToArrayLength (int[] toSortArray, int i)
+        private static int FindMaxIndexFromXToArrayLength (T[] toSortArray, int i)
         {
             int maxIDX = i;
             for (int j = i + 1; j < toSortArray.Length; j++)
-                if (toSortArray[j] > toSortArray[maxIDX])
+                if (IsBiggerThan( toSortArray[j] , toSortArray[maxIDX]))
                     maxIDX = j;
             return maxIDX;
         }
--- GrokkingAlgoTests/ChapterTwoTest/SelectionSortTests.cs
+++ GrokkingAlgoTests/ChapterTwoTest/SelectionSortTests.cs
@@ -8,29 +8,38 @@ namespace GrokkingAlgoTests.ChapterTwoTest
     public class SelectionSortTests
     {
         [Test]
-        public void RandArraySortedASC () {
-            int[] array = {5,6,9,10,3,7,2,1,8,0,4,100,-100 };
-            array = SelectionSort.SortArray(array);
-            Printarray(array);
-            // 012345678910
-            Assert.IsTrue(array.SequenceEqual(new int[]{ -100,0,1,2,3,4,5,6,7,8,9,10,100 }));
+        public void RandArraySortedASC ()
+        {
+            int[] array = { 5, 6, 9, 10, 3, 7, 2, 1, 8, 0, 4, 100, -100 };
+            SelectionSort<int>.SortArray(array);
+            Assert.IsTrue(array.SequenceEqual(new int[] { -100, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100 }));
+            // Char 
+            char[] charArray = { 'A', 'B', 'Z', 'N', 'M', 'a', 'C' };
+            SelectionSort<char>.SortArray(charArray);
+            Printarray(charArray);
+            Assert.IsTrue(charArray.SequenceEqual(new char[] { 'A', 'B', 'C', 'M', 'N', 'Z', 'a' }));
         }
 
         [Test]
         public void RandArraySortedDESC ()
         {
             int[] array = { 5, 6, 9, 10, 3, 7, 2, 1, 8, 0, 4, 100, -100 };
-            array = SelectionSort.SortArray(array , false);
-            Printarray(array);
-            Assert.IsTrue(array.SequenceEqual(new int[] { 100,10,9,8,7,6,5,4,3,2,1, 0, -100}));
+            SelectionSort<int>.SortArray(array, false);
+            Assert.IsTrue(array.SequenceEqual(new int[] { 100, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -100 }));
+            // Char 
+            char[] charArray = { 'A', 'B', 'Z', 'N', 'M', 'a', 'C' };
+            SelectionSort<char>.SortArray(charArray,false);
+            Printarray(charArray);
+            Assert.IsTrue(charArray.SequenceEqual(new char[] {  'a', 'Z', 'N', 'M', 'C', 'B', 'A' }));
         }
 
-        private static void Printarray(int[] arr)
+        private static void Printarray<T> (T[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
             {
-                Console.Write(arr[i]+",");
+                Console.Write(arr[i] + ",");
             }
         }
     }
+
 }
